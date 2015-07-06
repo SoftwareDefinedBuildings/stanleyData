8e3d38cdec59b9c9df0dd22051ae3462dfd60018
Arka Bhattacharya <arkaaloke@gmail.com>
2015-07-06 14:27:34 -0700

    added ground truth and a small timeseries library to perform required manipulations

---
+++ timeseries.py
@@ -0,0 +1,85 @@
+import sys
+import os
+from datetime import datetime
+from datetime import timedelta
+import numpy
+
+class Timeseries:
+	series = {}
+	name = None
+	bucket = 15
+	periods = {}
+	def __init__(self, fileName, name, bucketSize , offset=0):
+		
+		lines = open(fileName).readlines()
+		data = {}
+		for line in lines:
+			parts = line.strip().split(',')
+			t = datetime.fromtimestamp(float(parts[0].strip())) - offset
+			v = float(parts[1].strip())
+			data[t] = v
+
+		self.original_ts = pd.Series(data)
+		self.resampled_ts = self.resample( self.bucket )
+		
+	def resample(self, bucket):
+		newts = self.original_ts.resample(str(bucket) + 'min', how=np.mean)
+		resampled_ts = newts.interpolate()
+		return resampled_ts
+	
+	def addPeriod(self, index, startDate, endDate):
+		if index not in self.periods:
+			self.periods[index] = []
+		self.periods[index].append( (startDate, endDate ) )
+
+	def getBucket(self):
+		return self.bucket
+	
+	def setBucket(self,bucket):
+		self.bucket = bucket
+		self.resampled_ts = self.resample( self.bucket )
+
+	'''
+	Returns a dict with required values
+	'''
+
+	def filterPeriod( self, periodNum ) :
+		returnData = {}
+		indices = self.resampled_ts.index
+		for i in range(len(self.periods[periodNum])):
+			startTime = self.periods[periodNum][0]
+			endTime = self.periods[periodNum][1]
+			result = {}
+			for j in range(len(indices)):
+				t = indices[j].to_datetime()
+				if t < startTime or t > endTime:
+					continue
+				result[t] = self.resampled_ts[j]
+
+			returnData.update(result)
+
+		return returnData
+
+	'''
+	Returns mean, std for each day over a period
+	'''
+
+	def getPerDayMean( self, periodNum ):
+		data = self.filterPeriod( periodNum )
+		valuesByDay = {}
+		for t in data:
+			if t.day not in valuesByDay:
+				valuesByDay[t.day] = []
+			valuesByDay[t.day].append(data[t])
+
+		resultsByDay = {}
+		for day in valuesByDay:
+			''' 
+			Put a check in place to see whether you have a full day of data
+			'''
+			if len(valuesByDay[day]) != ( 24 * 60 ) / self.bucket :
+				continue
+			resultsByDay[day] = ( numpy.mean(valuesByDay[day]) , numpy.std(valuesByDay[day]) )
+
+		return resultsByDay
+		
